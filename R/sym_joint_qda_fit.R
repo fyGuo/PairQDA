22b8d7c34b7a9a4cc43bd8ed118ac5829d43d9f8
fyGuo <fuyuguo98@163.com>
2023-08-03 20:02:39 +0800

    Add a shuffling approach to symmetrical joint QDA

--- R/sym_joint_qda_fit.R
+++ R/sym_joint_qda_fit.R
@@ -1,4 +1,4 @@
-#' Symmetric Joint QDA (fitting model)
+#' Fitting Symmetric Joint QDA model (No shuffling version)
 #' @import dplyr
 #'
 #' @param train_data A data.frame for the training dataset
@@ -13,17 +13,8 @@
 #'
 #' @export
 
-#' @examples
-#' library(dplyr)
-#' library(mvtnorm)
-#' data(HearingLoss_simu)
-#' fit <- sym_joint_qda_fit(HearingLoss_simu,
-#'                   id = "id",
-#'                   method = "separate")
-#' test_data_X <- HearingLoss_simu[1,] %>% dplyr::select(-"Label_1", -"Label_2",-"id")
-#' sym_joint_qda_predict(fit, test_data_X)
 
-sym_joint_qda_fit <- function(train_data,
+sym_joint_qda_fit_NoShuffling <- function(train_data,
                           id = "SID",
                           Y1 = "Label_1",
                           Y2 = "Label_2",
@@ -113,5 +104,182 @@ sym_joint_qda_fit <- function(train_data,
        prior_weight = prior_weight,
        k = k) %>% return()
 }
+
+
+
+
+#' Fitting Symmetric Joint QDA model (shuffling version)
+#' @import dplyr
+#'
+#' @param train_data A data.frame for the training dataset
+#' @param id A character for the column name which stores id.
+#' @param Y1 A character shows the column name of label 1.
+#' @param Y2 A character shows the column name of label 2.
+#' @param X1 A string of characters for the column names of predictor set 1.
+#' @param X2 A string of characters for the column names of predictor set 2.
+#' @param k The number of classifications.
+#' @param S The number of shuffles.
+#' @param seed The random seed.
+#' @returns A list to store model parameters.
+#'
+#' @export
+
+
+sym_joint_qda_fit_Shuffling  <- function(train_data,
+                              id = "SID",
+                              Y1 = "Label_1",
+                              Y2 = "Label_2",
+                              X1 = c("T500_1", "T1K_1", "T2K_1", "T3K_1",
+                                     "T4K_1", "T6K_1", "T8K_1"),
+                              X2 = c("T500_2", "T1K_2", "T2K_2", "T3K_2",
+                                     "T4K_2", "T6K_2", "T8K_2"),
+                              k = 4,
+                              S = 10,
+                              seed) {
+
+  set.seed(seed)
+
+  prior_weight <- matrix(0, nrow = 4, ncol = 4)
+
+  mu_list_ear1 <- mu_list_ear2 <- vector(mode = "list", length = k)
+  var_list_ear1 <- var_list_ear2 <- vector(mode = "list", length = k)
+
+  for (s in 1:S) {
+    train_data_s <- train_data
+   for (i in 1:dim(train_data_s)[1]) {
+     pair <- sample(c(1,2), size = 2, replace = F)
+
+     # if after the location did not change we do nothing
+     # otherwise, we switch Y1 and Y2, X1 and X2
+     if (pair[1] == 2 & pair[2] == 1) {
+       train_data_s[i,X1] <- train_data[i,X2]
+       train_data_s[i,X2] <- train_data[i,X1]
+
+       train_data_s[i,Y1] <- train_data[i,Y2]
+       train_data_s[i,Y2] <- train_data[i,Y1]
+
+     }
+   }
+
+    # arrange the training dataset
+    train_data_ear1 <- train_data_s[,c(id, Y1, X1)] %>% dplyr::mutate(ear = 1)
+    colnames(train_data_ear1) <- c("id", "Y", paste("S", 1:length(X1), sep =""), "ear")
+
+
+    train_data_ear2 <- train_data_s[,c(id, Y2, X2)] %>% dplyr::mutate(ear = 2)
+    colnames(train_data_ear2) <- c("id", "Y", paste("S", 1:length(X2), sep =""), "ear")
+
+    # prior weights
+    weight_matrix <- +
+      table(train_data_ear1$Y, train_data_ear2$Y, dnn = c("Ear1", "Ear2"))  %>%
+      prop.table() %>%
+      as.matrix()
+
+    prior_weight <- prior_weight + weight_matrix/S
+
+
+    # excluding rows with missing values
+    train_data_ear1 <- train_data_ear1[stats::complete.cases(train_data_ear1),]
+    train_data_ear2 <- train_data_ear2[stats::complete.cases(train_data_ear2),]
+
+    # create a pooled dataset
+    train_data_pooled <- rbind(train_data_ear1, train_data_ear2)
+
+
+    # excluding rows with missing values
+    train_data_ear1 <- train_data_ear1[stats::complete.cases(train_data_ear1),]
+    train_data_ear2 <- train_data_ear2[stats::complete.cases(train_data_ear2),]
+
+    # calculating the mean list
+    for (h in 1:k) {
+      mu <- train_data_pooled %>% dplyr::filter(.data[["Y"]] == h) %>%
+        dplyr::select(paste("S", 1:length(X1), sep ="")) %>%
+        colMeans(na.rm = T)
+
+      if (s == 1) {mu_list_ear1[[h]] <- mu_list_ear2[[h]] <- mu/S}
+      else {mu_list_ear1[[h]] <- mu_list_ear1[[h]] +  mu/S
+            mu_list_ear2[[h]] <- mu_list_ear2[[h]] +  mu/S}
+    }
+
+    # calculating he variance list
+    for (h in 1:k) {
+      var <-
+        train_data_pooled %>% dplyr::filter(.data[["Y"]] == h) %>%
+        dplyr::select(paste("S", 1:length(X1), sep ="")) %>%
+        stats::var(na.rm = T)
+      if (s == 1) {var_list_ear1[[h]] <- var_list_ear2[[h]] <- var/S}
+      else {var_list_ear1[[h]] <- var_list_ear1[[h]] + var/S
+            var_list_ear2[[h]] <- var_list_ear2[[h]] + var/S}
+    }
+
+  }
+
+  list(mu_list_ear1 = mu_list_ear1,
+       var_list_ear1 = var_list_ear1,
+       mu_list_ear2 = mu_list_ear2,
+       var_list_ear2 = var_list_ear2,
+       prior_weight = prior_weight,
+       k = k) %>% return()
+}
+
+
+#' Fitting Symmetric Joint QDA model
+#' @import dplyr
+#'
+#' @param train_data A data.frame for the training dataset
+#' @param id A character for the column name which stores id.
+#' @param Y1 A character shows the column name of label 1.
+#' @param Y2 A character shows the column name of label 2.
+#' @param X1 A string of characters for the column names of predictor set 1.
+#' @param X2 A string of characters for the column names of predictor set 2.
+#' @param k The number of classifications.
+#' @param Shuffle Whether to use data shuffling before the QDA methods.
+#' @param seed The random seed. Only applicable when using Shuffling.
+#' @param S The number of shuffles. Only applicable when using Shuffling.
+#' @param method The estimation method. Only applicable when not using Shuffling.
+#' @returns A list to store model parameters.
+#'
+#' @export
+
+#' @examples
+#' library(dplyr)
+#' library(mvtnorm)
+#' data(HearingLoss_simu)
+#' #Use the Shuffling method
+#' fit <- sym_joint_qda_fit(HearingLoss_simu,
+#'                   id = "id",
+#'                   Shuffle = TRUE)
+#'
+#' #Use the Non-shuffling method
+#' fit <- sym_joint_qda_fit(HearingLoss_simu,
+#'                   id = "id",
+#'                   Shuffle = FALSE,
+#'                   method = "pooled")
+
+sym_joint_qda_fit  <- function(train_data,
+                               id = "SID",
+                               Y1 = "Label_1",
+                               Y2 = "Label_2",
+                               X1 = c("T500_1", "T1K_1", "T2K_1", "T3K_1",
+                                      "T4K_1", "T6K_1", "T8K_1"),
+                               X2 = c("T500_2", "T1K_2", "T2K_2", "T3K_2",
+                                      "T4K_2", "T6K_2", "T8K_2"),
+                              k = 4,
+                              Shuffle = TRUE,
+                              seed = 123,
+                              S = 10,
+                              method = c("pooled", "separate")) {
+
+  if (Shuffle) {sym_joint_qda_fit_Shuffling(
+    train_data = train_data, id = id, Y1 = Y1, Y2 = Y2, X1 = X1,
+    X2 = X2, k = k, seed = seed, S = S
+  )}
+
+  else {sym_joint_qda_fit_NoShuffling(
+    train_data = train_data, id = id, Y1 = Y1, Y2 = Y2, X1 = X1,
+    X2 = X2, k = k, method = method)}
+}
+
+
 
 
--- R/sym_joint_qda_predict.R
+++ R/sym_joint_qda_predict.R
@@ -6,7 +6,7 @@
 #' @param test_data_X A vector of predictors from the test data
 #' @param X1 A string of characters for the column names of predictor set 1.
 #' @param X2 A string of characters for the column names of predictor set 2.
-#'
+#' @param Shuffle Whether to use data shuffling before the QDA methods.
 #' @returns Predicted individual level phenotype
 #'
 #' @export
@@ -15,18 +15,28 @@
 #' library(dplyr)
 #' library(mvtnorm)
 #' data(HearingLoss_simu)
+#' #Use the Shuffling method
 #' fit <- sym_joint_qda_fit(HearingLoss_simu,
 #'                   id = "id",
-#'                   method = "separate")
+#'                   Shuffle = TRUE)
 #' test_data_X <- HearingLoss_simu[1,] %>% dplyr::select(-"Label_1", -"Label_2",-"id")
-#' sym_joint_qda_predict(fit, test_data_X)
+#' sym_joint_qda_predict(fit, test_data_X, Shuffle = TRUE)
+#'
+#' # Use the Non-Shuffling method
+#' fit <- sym_joint_qda_fit(HearingLoss_simu,
+#'                   id = "id",
+#'                   Shuffle = FALSE,
+#'                   method = "pooled")
+#' test_data_X <- HearingLoss_simu[1,] %>% dplyr::select(-"Label_1", -"Label_2",-"id")
+#' sym_joint_qda_predict(fit, test_data_X, Shuffle = FALSE)
 
 sym_joint_qda_predict <- function(qda_model = NA,
                               test_data_X = NA,
                               X1 = c("T500_1", "T1K_1", "T2K_1", "T3K_1",
                                      "T4K_1", "T6K_1", "T8K_1"),
                               X2 = c("T500_2", "T1K_2", "T2K_2", "T3K_2",
-                                     "T4K_2", "T6K_2", "T8K_2")) {
+                                     "T4K_2", "T6K_2", "T8K_2"),
+                              Shuffle = TRUE) {
 
   # arrange the validation dataset
 
@@ -51,6 +61,40 @@ sym_joint_qda_predict <- function(qda_model = NA,
   predict_ear_1 <- index %% qda_model$k
   predict_ear_2 <- ceiling(index / qda_model$k)
   if (predict_ear_1 == 0) predict_ear_1 <- qda_model$k
-  predicted_valid <- paste(predict_ear_1, predict_ear_2, sep = "_")
-  predicted_valid%>% return()
+  prediction_1 <- paste(predict_ear_1, predict_ear_2, sep = "_")
+  P_1 <- max(probability_log)
+
+  if (Shuffle == FALSE) return(prediction_1)
+
+  else {
+    # arrange the validation dataset
+
+    test_data_ear1_X <- test_data_X[,X2]
+
+    colnames(test_data_ear1_X) <- c(paste("S", 1:length(X2), sep =""))
+
+    test_data_ear2_X <-test_data_X[,X1]
+
+    colnames(test_data_ear2_X) <- c(paste("S", 1:length(X1), sep =""))
+
+    probability_log <- matrix(NA, nrow = qda_model$k, ncol = qda_model$k)
+    for (ear1 in 1:qda_model$k) {
+      for (ear2 in 1:qda_model$k) {
+        probability_log[ear1, ear2] <-
+          dmvnorm(test_data_ear1_X, qda_model$mu_list_ear1[[ear1]], sigma = qda_model$var_list_ear1[[ear1]], log = T) +
+          dmvnorm(test_data_ear2_X, qda_model$mu_list_ear2[[ear2]], sigma = qda_model$var_list_ear2[[ear2]], log = T) +
+          log(qda_model$prior_weight[ear1,ear2])
+      }
+    }
+    index <- which.max(probability_log)
+    predict_ear_1 <- index %% qda_model$k
+    predict_ear_2 <- ceiling(index / qda_model$k)
+    if (predict_ear_1 == 0) predict_ear_1 <- qda_model$k
+
+    prediction_2 <- paste(predict_ear_1, predict_ear_2, sep = "_")
+    P_2 <- max(probability_log)
+
+    if (P_1 >= P_2) {return (prediction_1)}
+    else {return(prediction_2)}
+  }
 }
